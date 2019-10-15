5866bd12e8b3dd55706465c8f98990528262bbab
atj2097 <52180742+atj2097@users.noreply.github.com>
2019-10-15 15:16:03 -0400

    Add files via upload

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by God on 10/15/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
